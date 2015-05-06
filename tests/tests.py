53e1adc77c839f4690d742b93657c6ff4c87554e
Mark Finger <markfinger@gmail.com>
2015-05-06 22:08:53 +1000

    Added an options testcase

--- tests/tests.py
+++ tests/tests.py
@@ -10,9 +10,13 @@ TEST_CSS_FILE = os.path.join(os.path.dirname(__file__), 'test.css')
 
 class TestDjangoFrontendTools(unittest.TestCase):
     def test_autoprefixer_can_process_css_string(self):
-        css = '.foo { -webkit-border-radius: 100%; border-radius: 100%; }'
+        css = '.foo { -moz-border-radius: 100%; border-radius: 100%; }'
         expected = '.foo { border-radius: 100%; }'
         self.assertEqual(autoprefixer(css), expected)
+
+    def test_autoprefixer_can_accept_an_options_dict(self):
+        css = '.foo { -moz-border-radius: 100%; border-radius: 100%; }'
+        self.assertEqual(autoprefixer(css, options={'browsers': ['Firefox 3']}), css)
 
     def test_autoprefixer_can_process_css_file(self):
         expected = '.foo { border-radius: 100%; }'
